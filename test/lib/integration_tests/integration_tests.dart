e522f3766c89882fefc0ff4caaf6e9244a617831
Romulo Carvalho <oakromulo@gmail.com>
2019-06-18 15:29:58 -0300

    feature/assert-test
    - assert test added to verify basic functioning of the localstorage "Item" methods, namely: `deleteItem`, `getItem` and `setItem`

--- test/lib/integration_tests/integration_tests.dart
+++ test/lib/integration_tests/integration_tests.dart
@@ -3,11 +3,57 @@
 /// @nodoc
 library integration_tests;
 
+import 'dart:math';
+
+import 'package:localstorage/localstorage.dart';
+
 class IntegrationTests {
   static Future<String> assertTest() async {
-    await Future.delayed(Duration(seconds: 5));
+    final key = _randomTxt;
 
-    return 'assert test completed successfully';
+    // write a sample map
+    await (() async {
+      final storage = LocalStorage(key);
+
+      await storage.ready;
+
+      await storage.setItem(key, {'$key': key});
+    })();
+
+    // assert sample map can be read correctly
+    await (() async {
+      final storage = LocalStorage(key);
+
+      await storage.ready;
+
+      Map<String, dynamic> ref = {'$key': key};
+      Map<String, dynamic> res = await storage.getItem(key);
+
+      assert(res[key] == ref[key]);
+    })();
+
+    // delete sample map
+    await (() async {
+      final storage = LocalStorage(key);
+
+      await storage.ready;
+
+      await storage.deleteItem(key);
+    })();
+
+    // assert map cannot be read anymore
+    await (() async {
+      final storage = LocalStorage(key);
+
+      await storage.ready;
+
+      assert(await storage.getItem(key) == null);
+    })();
+
+    const msg = 'assert test completed successfully';
+
+    print(msg);
+    return msg;
   }
 
   static Future<String> tputTest() async {
@@ -23,6 +69,15 @@ class IntegrationTests {
 
     assert(eventTput >= 100);
 
-    return 'tput test completed successfully';
+    const msg = 'tput test completed successfully';
+
+    print(msg);
+    return msg;
+  }
+
+  static String get _randomTxt {
+    final random = Random.secure();
+
+    return random.nextInt(1000000000).toString();
   }
 }
